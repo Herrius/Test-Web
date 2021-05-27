5a901865b650c3a995080bf9e0a02949ee9840a5
JcEC13 <64324020+JcEC13@users.noreply.github.com>
2021-05-27 17:47:40 -0500

    Update procesar.php
    Agregar la consulta para poder contar solo los correspondientes a un salon determinado

--- greporteaula/procesar.php
+++ greporteaula/procesar.php
@@ -11,15 +11,16 @@
     }
 
     //$NRC=$_POST['NRC'];
+    $NRC='8555';
 
-    $activo=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(activoreflexivo) AS ACTIVO FROM `tblresultados` WHERE activoreflexivo='Activo'"));
-    $reflexivo=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(activoreflexivo) AS REFLEXIVO FROM `tblresultados` WHERE activoreflexivo='Reflexivo'"));
-    $sensorial=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(sensorialintuitivo) AS SENSORIAL FROM `tblresultados` WHERE sensorialintuitivo='Sensorial'"));
-    $intuitivo=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(sensorialintuitivo) AS INTUITIVO FROM `tblresultados` WHERE sensorialintuitivo='Intuitivo'"));
-    $visual=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(visualverbal) AS VISUAL FROM `tblresultados` WHERE visualverbal='Visual'"));
-    $verbal=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(visualverbal) AS VERBAL FROM `tblresultados` WHERE visualverbal='Verbal'"));
-    $secuencial=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(secuencialglobal) AS SECUENCIAL FROM `tblresultados` WHERE secuencialglobal='Secuencial'"));
-    $global=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(secuencialglobal) AS GLOBAL FROM `tblresultados` WHERE secuencialglobal='Global'"));
+    $activo=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(activoreflexivo) AS ACTIVO FROM `tblresultados` WHERE codestudiante IN(SELECT codestudiante FROM tblconsulta WHERE NRC='$NRC') AND activoreflexivo='Activo'"));
+    $reflexivo=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(activoreflexivo) AS REFLEXIVO FROM `tblresultados` WHERE codestudiante IN(SELECT codestudiante FROM tblconsulta WHERE NRC='$NRC') AND activoreflexivo='Reflexivo'"));
+    $sensorial=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(sensorialintuitivo) AS SENSORIAL FROM `tblresultados` WHERE codestudiante IN(SELECT codestudiante FROM tblconsulta WHERE NRC='$NRC') AND sensorialintuitivo='Sensorial'"));
+    $intuitivo=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(sensorialintuitivo) AS INTUITIVO FROM `tblresultados` WHERE codestudiante IN(SELECT codestudiante FROM tblconsulta WHERE NRC='$NRC') AND sensorialintuitivo='Intuitivo'"));
+    $visual=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(visualverbal) AS VISUAL FROM `tblresultados` WHERE codestudiante IN(SELECT codestudiante FROM tblconsulta WHERE NRC='$NRC') AND visualverbal='Visual'"));
+    $verbal=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(visualverbal) AS VERBAL FROM `tblresultados` WHERE codestudiante IN(SELECT codestudiante FROM tblconsulta WHERE NRC='$NRC') AND visualverbal='Verbal'"));
+    $secuencial=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(secuencialglobal) AS SECUENCIAL FROM `tblresultados` WHERE codestudiante IN(SELECT codestudiante FROM tblconsulta WHERE NRC='$NRC') AND secuencialglobal='Secuencial'"));
+    $global=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(secuencialglobal) AS GLOBAL FROM `tblresultados` WHERE codestudiante IN(SELECT codestudiante FROM tblconsulta WHERE NRC='$NRC') AND secuencialglobal='Global'"));
 
     $data= array(
         array(
